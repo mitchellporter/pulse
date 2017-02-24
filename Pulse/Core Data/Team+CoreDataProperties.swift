ff36f9395867592883c949f21fb1a194ab2d9b6d
Mitchell Porter <mitchellporter@gmail.com>
2017-02-24 13:30:20 -0800

    model updates + created team model

---
+++ Pulse/Core Data/Team+CoreDataProperties.swift
@@ -0,0 +1,42 @@
+//
+//  Team+CoreDataProperties.swift
+//  Pulse
+//
+//  Created by Mitchell Porter on 2/24/17.
+//  Copyright © 2017 Mentor Ventures, Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Team {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Team> {
+        return NSFetchRequest<Team>(entityName: "Team");
+    }
+
+    @NSManaged public var objectId: String
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var updatedAt: Date?
+    @NSManaged public var domain: String
+    @NSManaged public var members: NSSet
+
+}
+
+// MARK: Generated accessors for members
+extension Team {
+
+    @objc(addMembersObject:)
+    @NSManaged public func addToMembers(_ value: User)
+
+    @objc(removeMembersObject:)
+    @NSManaged public func removeFromMembers(_ value: User)
+
+    @objc(addMembers:)
+    @NSManaged public func addToMembers(_ values: NSSet)
+
+    @objc(removeMembers:)
+    @NSManaged public func removeFromMembers(_ values: NSSet)
+
+}
