7b17f68bcd27cd9b73a9204aa008e5a3da1a8c8e
Mitchell Porter <mitchellporter@gmail.com>
2017-02-27 16:46:42 -0800

    created date extensions

--- Pulse/Core Data/Task+CoreDataClass.swift
+++ Pulse/Core Data/Task+CoreDataClass.swift
@@ -100,32 +100,3 @@ extension Task: PulseType {
     }
 }
 
-extension Date {
-    static func from(_ ISOString: String) -> Date? {
-        let formatter = DateFormatter()
-        
-        // Format 1
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        if let parsedDate = formatter.date(from: ISOString) {
-            return parsedDate
-        }
-        
-        // Format 2
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:SSSZ"
-        if let parsedDate = formatter.date(from: ISOString) {
-            return parsedDate
-        }
-        
-        // Couldn't parsed with any format. Just get the date
-        let splitedDate = ISOString.components(separatedBy: "T")
-        if splitedDate.count > 0 {
-            formatter.dateFormat = "yyyy-MM-dd"
-            if let parsedDate = formatter.date(from: splitedDate[0]) {
-                return parsedDate
-            }
-        }
-        return nil
-    }
-}
-
-
