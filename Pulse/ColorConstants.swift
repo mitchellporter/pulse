e53d372b88440f1a153aad79405665e13de53223
Allen Hurst <avhurst@gmail.com>
2017-02-28 17:32:11 -0800

    Started creating color constants file.

---
+++ Pulse/ColorConstants.swift
@@ -0,0 +1,12 @@
+//
+//  ColorConstants.swift
+//  Pulse
+//
+//  Created by Design First Apps on 2/28/17.
+//  Copyright © 2017 Mentor Ventures, Inc. All rights reserved.
+//
+
+import UIKit
+
+let createTaskBackgroundColor: UIColor = UIColor("1FA865")
+let mainBackgroundColor: UIColor = UIColor("042C4A")
