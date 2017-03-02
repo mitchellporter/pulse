8a5950d40d4d873f32ecbd0d1777d0037f657988
Allen Hurst <avhurst@gmail.com>
2017-03-02 13:35:07 -0800

    Added tableView for each view controller in TaskViewController, and set up MyTasks and CreatedTasks sections to segue to correct view controller.

--- Pulse/MyTasksViewController.swift
+++ Pulse/MyTasksViewController.swift
@@ -14,17 +14,10 @@ class MyTasksViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-//    var fetchedResultsController: NSFetchedResultsController<Task>!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setupTableView()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
         
     private func setupTableView() {
@@ -33,35 +26,16 @@ class MyTasksViewController: UIViewController {
         self.tableView.register(cell, forCellReuseIdentifier: "taskCell")
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 70
-        self.tableView.contentInset = UIEdgeInsets(top: 18, left: 0, bottom: 0, right: 0)
+//        self.tableView.contentInset = UIEdgeInsets(top: 18, left: 0, bottom: 0, right: 0)
         self.tableView.backgroundColor = mainBackgroundColor
         self.tableView.delegate = self
         self.tableView.dataSource = self
     }
 
-//    func configure(cell: UITableViewCell, at indexPath: IndexPath) {
-//        _ = self.fetchedResultsController.object(at: indexPath)
-//        // Setup cell
-//    }
-    
-//    private func setupCoreData() {
-//        let request: NSFetchRequest<Task> = Task.createFetchRequest()
-//        let sort = NSSortDescriptor(key: "createdAt", ascending: false)
-//        request.sortDescriptors = [sort]
-//        let fetchedResultsController: NSFetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType), sectionNameKeyPath: nil, cacheName: nil)
-//        fetchedResultsController.delegate = self
-//        self.fetchedResultsController = fetchedResultsController
-//        self.tableViewDatasource.fetchedResultsController = fetchedResultsController
-//        // TODO
-//        self.updatePredicate(for: "")
-//    }
-//    
-//    private func updatePredicate(for type: String) {
-//        // TODO
-//        let predicate = NSPredicate(format: "ANY projects.objectId == ")
-//        self.tableViewDatasource.fetchedResultsController.fetchRequest.predicate = predicate
-//    }
-    
+    func configure(cell: UITableViewCell, at indexPath: IndexPath) {
+        // Setup cell
+        cell.contentView.backgroundColor = self.tableView.backgroundColor
+    }
 }
 
 extension MyTasksViewController: UITableViewDataSource {
@@ -77,9 +51,7 @@ extension MyTasksViewController: UITableViewDataSource {
         guard let cell: TaskCell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as? TaskCell else {
             return tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
         }
-        
-        
-        
+        self.configure(cell: cell, at: indexPath)
         return cell
     }
 }
@@ -87,14 +59,14 @@ extension MyTasksViewController: UITableViewDataSource {
 extension MyTasksViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let segueID: String = self.modeSelected == .myTasks ? "viewTask" : "editTask"
-//        self.performSegue(withIdentifier: segueID, sender: nil)
+        guard let taskVC: TaskViewController = self.parent as? TaskViewController else { return }
+        taskVC.performSegue(withIdentifier: "viewTask", sender: nil)
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let header: TaskSectionHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "taskHeader") as? TaskSectionHeader else { return tableView.dequeueReusableHeaderFooterView(withIdentifier: "taskHeader") }
-        //        header.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: tableView.frame.width, height: 30))
         header.load(status: .inProgress)
+        header.contentView.backgroundColor = self.tableView.backgroundColor
         return header
     }
     
@@ -104,58 +76,58 @@ extension MyTasksViewController: UITableViewDelegate {
     
 }
 
-//extension MyTasksViewController: NSFetchedResultsControllerDelegate {
-//    
-//    /*
-//     Assume self has a property 'tableView' -- as is the case for an instance of a UITableViewController
-//     subclass -- and a method configureCell:atIndexPath: which updates the contents of a given cell
-//     with information from a managed object at the given index path in the fetched results controller.
-//     */
-//    
-//    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        self.tableView.beginUpdates()
-//    }
-//    
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-//        switch(type) {
-//        case .insert:
-//            self.tableView.insertSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
-//            break
-//            
-//        case .delete:
-//            self.tableView.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
-//            break
-//        default:
-//            break
-//        }
-//    }
-//    
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-//        let tableView: UITableView = self.tableView
-//        guard let indexPath: IndexPath = indexPath else { return }
-//        switch(type) {
-//            
-//        case .insert:
-//            tableView.insertRows(at: [indexPath], with: .fade)
-//            break
-//            
-//        case .delete:
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//            break
-//            
-//        case .update:
-//            guard let cell: UITableViewCell = self.tableView.cellForRow(at: indexPath) else { break }
-//            self.configure(cell: cell, at: indexPath)
-//            break
-//            
-//        case .move:
-//            self.tableView.deleteRows(at: [indexPath], with: .fade)
-//            self.tableView.insertRows(at: [indexPath], with: .fade)
-//            break
-//        }
-//    }
-//    
-//    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        self.tableView.endUpdates()
-//    }
-//}
+extension MyTasksViewController: NSFetchedResultsControllerDelegate {
+    
+    /*
+     Assume self has a property 'tableView' -- as is the case for an instance of a UITableViewController
+     subclass -- and a method configureCell:atIndexPath: which updates the contents of a given cell
+     with information from a managed object at the given index path in the fetched results controller.
+     */
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch(type) {
+        case .insert:
+            self.tableView.insertSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+            break
+            
+        case .delete:
+            self.tableView.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+            break
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        let tableView: UITableView = self.tableView
+        guard let indexPath: IndexPath = indexPath else { return }
+        switch(type) {
+            
+        case .insert:
+            tableView.insertRows(at: [indexPath], with: .fade)
+            break
+            
+        case .delete:
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            break
+            
+        case .update:
+            guard let cell: UITableViewCell = self.tableView.cellForRow(at: indexPath) else { break }
+            self.configure(cell: cell, at: indexPath)
+            break
+            
+        case .move:
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            self.tableView.insertRows(at: [indexPath], with: .fade)
+            break
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.endUpdates()
+    }
+}
--- Pulse/UpdatesViewController.swift
+++ Pulse/UpdatesViewController.swift
@@ -7,29 +7,128 @@
 //
 
 import UIKit
+import CoreData
 
 class UpdatesViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.setupTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func setupTableView() {
+        self.tableView.register(TaskSectionHeader.self, forHeaderFooterViewReuseIdentifier: "taskHeader")
+        let cell: UINib = UINib(nibName: "TaskCell", bundle: nil)
+        self.tableView.register(cell, forCellReuseIdentifier: "taskCell")
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 70
+//        self.tableView.contentInset = UIEdgeInsets(top: 18, left: 0, bottom: 0, right: 0)
+        self.tableView.backgroundColor = mainBackgroundColor
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-    */
+    
+    func configure(cell: UITableViewCell, at indexPath: IndexPath) {
+        // Setup cell
+        cell.contentView.backgroundColor = self.tableView.backgroundColor
+    }
+}
 
+extension UpdatesViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: TaskCell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as? TaskCell else {
+            return tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        }
+        self.configure(cell: cell, at: indexPath)
+        return cell
+    }
+}
+
+extension UpdatesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let taskVC: TaskViewController = self.parent as? TaskViewController else { return }
+//        taskVC.performSegue(withIdentifier: "viewTask", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header: TaskSectionHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "taskHeader") as? TaskSectionHeader else { return tableView.dequeueReusableHeaderFooterView(withIdentifier: "taskHeader") }
+        header.load(status: .inProgress)
+        header.contentView.backgroundColor = self.tableView.backgroundColor
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+}
+
+extension UpdatesViewController: NSFetchedResultsControllerDelegate {
+    
+    /*
+     Assume self has a property 'tableView' -- as is the case for an instance of a UITableViewController
+     subclass -- and a method configureCell:atIndexPath: which updates the contents of a given cell
+     with information from a managed object at the given index path in the fetched results controller.
+     */
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch(type) {
+        case .insert:
+            self.tableView.insertSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+            break
+            
+        case .delete:
+            self.tableView.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+            break
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        let tableView: UITableView = self.tableView
+        guard let indexPath: IndexPath = indexPath else { return }
+        switch(type) {
+            
+        case .insert:
+            tableView.insertRows(at: [indexPath], with: .fade)
+            break
+            
+        case .delete:
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            break
+            
+        case .update:
+            guard let cell: UITableViewCell = self.tableView.cellForRow(at: indexPath) else { break }
+            self.configure(cell: cell, at: indexPath)
+            break
+            
+        case .move:
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            self.tableView.insertRows(at: [indexPath], with: .fade)
+            break
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.endUpdates()
+    }
 }
