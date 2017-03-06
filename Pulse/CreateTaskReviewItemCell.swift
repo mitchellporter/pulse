2e31446d1d7928cbcab1849335a6aeb0d2ddd2c4
Allen Hurst <avhurst@gmail.com>
2017-03-06 13:22:27 -0800

    Updated CreatTaskReviewController to have edit buttons next to description and items.

--- Pulse/CreateTaskReviewItemCell.swift
+++ Pulse/CreateTaskReviewItemCell.swift
@@ -17,15 +17,6 @@ class CreateTaskReviewItemCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        self.setupDot()
-    }
-
-    private func setupDot() {
-        let frame: CGRect = CGRect(x: 22, y: 18, width: 12, height: 12)
-        let path: UIBezierPath = UIBezierPath(ovalIn: frame)
-        self.dot.path = path.cgPath
-        self.dot.fillColor = UIColor.white.cgColor
-        self.layer.addSublayer(self.dot)
     }
     
 }
