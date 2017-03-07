d33dc8cb47b4934303eaa6869236f74bfa67ac1b
Allen Hurst <avhurst@gmail.com>
2017-03-07 13:42:27 -0800

    Added selected button state for CreateTaskUpdatesViewController.

--- Pulse/CreateTaskUpdatesViewController.swift
+++ Pulse/CreateTaskUpdatesViewController.swift
@@ -35,6 +35,12 @@ class CreateTaskUpdatesViewController: UIViewController {
         self.setupAppearance()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.setButtonStateFromDictionary()
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         if self.isMovingFromParentViewController || self.isBeingDismissed {
@@ -46,13 +52,66 @@ class CreateTaskUpdatesViewController: UIViewController {
     
     private func setupAppearance() {
         self.view.backgroundColor = createTaskBackgroundColor
+        
+        self.addCheckOn(button: self.mondayButton)
+        self.addCheckOn(button: self.wednesdayButton)
+        self.addCheckOn(button: self.fridayButton)
     }
     
-    private func addRemoveInterval(_ interval: WeekDay) {
+    private func addCheckOn(button: UIButton) {
+        // Draw check on button
+        let pointOne: CGPoint = CGPoint(x: 8.73535156, y: 17.215332)
+        let pointTwo: CGPoint = CGPoint(x: 15.6796875, y: 23.4306641)
+        let pointThree: CGPoint = CGPoint(x: 24.8242188, y: 11)
+        
+        let checkLayer: CAShapeLayer = CAShapeLayer()
+        checkLayer.frame.size = CGSize(width: 34, height: 34)
+        checkLayer.frame.origin = CGPoint(x: 20, y: (button.frame.height/2) - (checkLayer.frame.height/2))
+        let checkPath: UIBezierPath = UIBezierPath()
+        checkPath.move(to: pointOne)
+        checkPath.addLine(to: pointTwo)
+        checkPath.addLine(to: pointThree)
+        checkLayer.path = checkPath.cgPath
+        checkLayer.fillColor = UIColor.clear.cgColor
+        checkLayer.strokeColor = createTaskBackgroundColor.cgColor
+        checkLayer.lineWidth = 4
+        
+        button.layer.addSublayer(checkLayer)
+    }
+    
+    private func setButtonStateFromDictionary() {
+        for day in self.selectedDays {
+            if day == .monday {
+                self.setSelected(state: true, button: self.mondayButton)
+            }
+            if day == .wednesday {
+                self.setSelected(state: true, button: self.wednesdayButton)
+            }
+            if day == .friday {
+                self.setSelected(state: true, button: self.fridayButton)
+            }
+        }
+    }
+    
+    private func addRemoveInterval(_ interval: WeekDay, for button: UIButton) {
         if self.selectedDays.contains(interval) {
             self.selectedDays.remove(interval)
+            self.setSelected(state: false, button: button)
         } else {
             self.selectedDays.insert(interval)
+            self.setSelected(state: true, button: button)
+        }
+    }
+    
+    private func setSelected(state: Bool, button: UIButton) {
+        if state {
+            // Set button selected state
+            button.setTitleColor(createTaskBackgroundColor, for: .normal)
+            button.backgroundColor = UIColor.white
+        } else {
+            // Remove button selected state
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.backgroundColor = UIColor.clear
         }
     }
     
@@ -70,11 +129,11 @@ class CreateTaskUpdatesViewController: UIViewController {
     
     @IBAction func dayButtonPressed(_ sender: UIButton) {
         if sender == self.mondayButton {
-            self.addRemoveInterval(.monday)
+            self.addRemoveInterval(.monday, for: sender)
         } else if sender == self.wednesdayButton {
-            self.addRemoveInterval(.wednesday)
+            self.addRemoveInterval(.wednesday, for: sender)
         } else if sender == self.fridayButton {
-            self.addRemoveInterval(.friday)
+            self.addRemoveInterval(.friday, for: sender)
         }
     }
     
