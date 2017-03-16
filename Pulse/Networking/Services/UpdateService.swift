1eff63a3823d18985f4aa0d3daf49526f4be8bae
Mitchell Porter <mitchellporter@gmail.com>
2017-03-16 15:26:32 -0700

    commented out some code

--- Pulse/Networking/Services/UpdateService.swift
+++ Pulse/Networking/Services/UpdateService.swift
@@ -13,21 +13,21 @@ typealias UpdateSuccessCompletion = (_ update: Update) -> Void
 typealias UpdatesSuccessCompletion = (_ updates: [Update]) -> Void
 
 struct UpdateService {
-    static func getUpdates(updateRequestId: String, offset: Int, success: @escaping UpdatesSuccessCompletion, failure: @escaping PulseFailureCompletion) {
-        NetworkingClient.sharedClient.request(target: .getUpdates(updateRequestId: updateRequestId, offset: offset), success: { (data) in
-            let json = JSON(data: data)
-            if json["success"].boolValue {
-                if let updatesJSON = json["updates"].arrayObject {
-                    var updates = [Update]()
-                    updatesJSON.forEach({ (updateJSON) in
-                        let update = Update.from(json: updateJSON as! [String : AnyObject], context: CoreDataStack.shared.context)
-                        updates.append(update)
-                    })
-                    success(updates)
-                }
-            }
-        }, failure: failure)
-    }
+//    static func getUpdates(updateRequestId: String, offset: Int, success: @escaping UpdatesSuccessCompletion, failure: @escaping PulseFailureCompletion) {
+//        NetworkingClient.sharedClient.request(target: .get(updateRequestId: updateRequestId, offset: offset), success: { (data) in
+//            let json = JSON(data: data)
+//            if json["success"].boolValue {
+//                if let updatesJSON = json["updates"].arrayObject {
+//                    var updates = [Update]()
+//                    updatesJSON.forEach({ (updateJSON) in
+//                        let update = Update.from(json: updateJSON as! [String : AnyObject], context: CoreDataStack.shared.context)
+//                        updates.append(update)
+//                    })
+//                    success(updates)
+//                }
+//            }
+//        }, failure: failure)
+//    }
     
     static func requestTaskUpdate(taskId: String, success: @escaping UpdateSuccessCompletion, failure: @escaping PulseFailureCompletion) {
 //        NetworkingClient.sharedClient.request(target: .requestTaskUpdate(taskId: taskId), success: { (data) in
