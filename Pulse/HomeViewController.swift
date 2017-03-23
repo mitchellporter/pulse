9181368c3e40821ff61c30cca2de6df57239cf01
Allen Hurst <avhurst@gmail.com>
2017-03-23 12:44:06 -0700

    Removed setupAppearance call.

--- Pulse/HomeViewController.swift
+++ Pulse/HomeViewController.swift
@@ -24,7 +24,6 @@ class HomeViewController: UIViewController {
             self.userField.isHidden = true
         }
         
-        self.setupAppearance()
     }
     
     override func viewDidAppear(_ animated: Bool) {
