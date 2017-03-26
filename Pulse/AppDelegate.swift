dea7c11a903edf59e51fd898daf7af52b3af46fd
Mitchell Porter <mitchellporter@gmail.com>
2017-03-25 17:03:05 -0700

    enabled socket connection

--- Pulse/AppDelegate.swift
+++ Pulse/AppDelegate.swift
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-//        SocketManager.sharedManager.connect(userId: User.currentUserId())
+        SocketManager.sharedManager.connect(userId: User.currentUserId())
 //        SocketManager.sharedManager.sendTestMessage()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
