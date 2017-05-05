af7072e049881fa3797e08054333ff9f8f6f956e
Allen Hurst <avhurst@gmail.com>
2017-05-04 18:03:15 -0700

    Added moving message bar on AddSubTaskViewController and created appearance  methods for SetUpdatesViewController.

--- Pulse/AddSubTaskViewController.swift
+++ Pulse/AddSubTaskViewController.swift
@@ -24,6 +24,16 @@ class AddSubTaskViewController: UIViewController {
         self.setupTableView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setupObserver()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.removeObserver()
+    }
+    
     private func setupTableView() {
         let cell: UINib = UINib(nibName: "CreateTaskItemCell", bundle: nil)
         self.tableView.register(cell, forCellReuseIdentifier: "CreateItemCell")
@@ -34,6 +44,30 @@ class AddSubTaskViewController: UIViewController {
         self.tableView.estimatedRowHeight = 70
         self.tableView.dataSource = self
         self.tableView.backgroundColor = self.view.backgroundColor
+    }
+    
+    private func setupObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    private func removeObserver() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        guard let keyboardEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? CGRect) else { return }
+        guard let keyboardDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        let resigning: Bool = notification.name == .UIKeyboardWillHide
+        let bottomSpace: CGFloat = resigning ? 0.0 : keyboardEndFrame.height
+        
+        UIView.animate(withDuration: keyboardDuration, delay: 0.0, options: [.curveLinear, .layoutSubviews], animations: {
+            self.bottomMenuBottomConstraint.constant = bottomSpace
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
     fileprivate func addNew(item: String) {
--- Pulse/SetUpdatesViewController.swift
+++ Pulse/SetUpdatesViewController.swift
@@ -25,6 +25,13 @@ class SetUpdatesViewController: UIViewController {
     
     @IBOutlet var dayButtons: [UIButton]!
     
+    var set: Set<String> = [] {
+        didSet {
+            self.enableNextButton(!set.isEmpty)
+            print(self.set)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,5 +41,80 @@ class SetUpdatesViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func selectButton(_ button: UIButton, _ selected: Bool) {
+        if selected {
+            button.backgroundColor = UIColor("1FC176")
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.layer.borderColor = UIColor.clear.cgColor
+            return
+        }
+        button.backgroundColor = UIColor.clear
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.layer.borderColor = UIColor("959595").cgColor
+    }
+    
+    private func enableNextButton(_ enabled: Bool) {
+        if enabled {
+            self.skipButton.backgroundColor = UIColor("3EAEFF")
+            self.skipButton.setTitle("NEXT", for: .normal)
+            self.skipButton.setTitleColor(UIColor.white, for: .normal)
+            self.skipButton.layer.borderColor = UIColor.clear.cgColor
+            return
+        }
+        self.skipButton.backgroundColor = UIColor.clear
+        self.skipButton.setTitle("SKIP", for: .normal)
+        self.skipButton.setTitleColor(UIColor.black, for: .normal)
+        self.skipButton.layer.borderColor = UIColor("454545").cgColor
+    }
+    
+    @IBAction func dayButtonPressed(_ sender: UIButton) {
+        guard let dayLabel: String = sender.titleLabel?.text else { return }
+        
+        var day: String = ""
+        
+        switch (dayLabel) {
+        case "MON":
+            day = "Monday"
+            break
+        case "TUE":
+            day = "Tuesday"
+            break
+        case "WED":
+            day = "Wednesday"
+            break
+        case "THUR":
+            day = "Thursday"
+            break
+        case "FRI":
+            day = "Friday"
+            break
+        case "SAT":
+            day = "Saturday"
+            break
+        default:
+            return
+        }
+        
+        if self.set.contains(day) {
+            self.set.remove(day)
+            self.selectButton(sender, false)
+            return
+        }
+        self.set.insert(day)
+        self.selectButton(sender, true)
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        if self.navigationController != nil {
+            _ = self.navigationController?.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toAddFiles", sender: nil)
     }
 }
